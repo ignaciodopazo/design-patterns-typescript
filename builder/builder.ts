355eb4cef70b43620179c1625945cff67e5b8f4a
ignacio <ignacio.dopazo11@gmail.com>
2022-02-13 17:19:05 -0300

    Add a builder pattern implementation

---
+++ builder/builder.ts
@@ -0,0 +1,58 @@
+type A = { tag: "A", data: any }
+type B = { tag: "B", data: any }
+type C = { tag: "C", data: any }
+
+// Token type
+type Token = A | B | C
+
+class Director {
+    private structure: Array<Token>
+    private builder: AbstractBuilder
+
+    constructor (builder: AbstractBuilder, structure: Array<Token>) {
+        this.structure = structure
+        this.builder = builder
+    }
+
+    public construct() {
+        this.structure.forEach(elem => {
+            this.builder.buildPart(elem)
+        })
+    }
+
+}
+
+/**
+ * Each concrete builder will have the buildPart method
+ * implemented with its concrete mechanism to represent
+ * each one of the tokens of the Director "structure".
+ */
+interface AbstractBuilder {
+    buildPart(x: Token): void
+}
+
+class ConcreteBuilder1<R> implements AbstractBuilder {
+
+    private result: R | undefined
+
+    public buildPart(x: Token): void {
+        switch (x.tag) {
+            case "A":
+                // representation of A
+                break;
+            case "B":
+                // representation of B
+                break;
+            case "C":
+                // representation of C
+                break;
+            default:
+                throw new Error("bad token type");
+        }
+    }
+
+    public getResult(): R | undefined {
+        return this.result
+    }
+
+}
